1b975ed920db006cbbc7285ef6bb6a60a9be51f4
oladee <deemajor230600@gmail.com>
2023-06-16 19:54:57 +0100

    stylin the whitepane

--- flask-server/frontend/src/components/sidebar/index.jsx
+++ flask-server/frontend/src/components/sidebar/index.jsx
@@ -1,28 +1,33 @@
 // import { useState } from 'react'
-import './style.scss';
-import { iconData } from './iconData';
-
+import "./style.scss";
+import { iconData } from "./iconData";
 
 const Sidebar = () => {
-    // const [btnstate, setBtnstate] = useState(false)
-    // function handleClick() {
-    //     setBtnstate = (btnstate >= !btnstate)
-    // }
-    // let toggleClass = btnstate ? 'active' : null;
+  // const [btnstate, setBtnstate] = useState(false)
+  // function handleClick() {
+  //     setBtnstate = (btnstate >= !btnstate)
+  // }
+  // let toggleClass = btnstate ? 'active' : null;
 
-    return (<>
-        <div className="sideContainer">
-            <div className="blackpane">
-                {iconData.map(x => {
-                    return (<img src={x.url} alt={x.altText} title={x.title} key={x.title}/>)
-                })}
-            </div>
-            <div className="whitepane">
-                
-            </div>
+  return (
+    <>
+      <div className="sideContainer">
+        <div className="blackpane">
+          {iconData.map((x) => {
+            return (
+              <img src={x.url} alt={x.altText} title={x.title} key={x.title} />
+            );
+          })}
         </div>
-    </>)
-            
-}
+        <div className="whitepane">
+          <div className="whitepane-heading">
+            <h1>Projects</h1>
+            <div className="plus">+</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
